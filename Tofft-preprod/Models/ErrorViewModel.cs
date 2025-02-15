98ed1e399cbf809f1f848358e52d115cb0774c32
Alexander Maschenko <mash.xander@outlook.com>
2025-02-16 00:32:22 +0300

    Add project files.

---
+++ Tofft-preprod/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tofft_preprod.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
